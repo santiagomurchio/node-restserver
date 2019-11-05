261830b552db462b81a673a1bdcea80d158c2602
santiagomurchio <santiagomurchio@gmail.com>
2019-11-05 15:16:44 -0300

    Removed connection string

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://santiago:JHJnCWKFOspmxPtM@smm-cluster0-hyrxf.mongodb.net/cafe'
+    urlDB = process.env.MONGO_CONNECTION_STR;
 }
 process.env.DB_URL = urlDB;
